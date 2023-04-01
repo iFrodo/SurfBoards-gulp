7d070718c4366960ff4b80c3dbe075c5be21054a
lFrodo <igor@azinet.ru>
2023-04-01 11:38:01 +0300

    fp corrections

--- src/js/onePageScroll.js
+++ src/js/onePageScroll.js
@@ -24,104 +24,13 @@ $(document).ready(function () {
 );
 $("[data-scroll-to]").on('click',(e) => {
   e.preventDefault();
-  const $this = $(e.currentTarget);
-  const target = $this.attr("data-scroll-to");
-  const reqSection = $(`[data-section-id = ${target}]`);
-
-  performTransition(reqSection.index());
+  // находим имя секции
+  const sectionName = $(e.currentTarget).attr('data-scroll-to');
+  // находим её индекс
+  const sectionIndex = $(`[data-section-id="${sectionName}"]`).index();
+  // пользуемся документацией библиотеки и находим там метод для перехода к секции
+  // там индекс начинается с одного, а в JS с нуля, поэтому добавляем к индексу + 1
+  // чтоб всё работало правильно
+  $.fn.fullpage.moveTo(sectionIndex + 1);
 });
 
-//----------------------------------------------------------------------------------------------------------//
-
-const sections = $("section");
-const display = $(".maincontent");
-let inScroll = false;
-sections.first().addClass("active");
-
-const performTransition = (sectionEq) => {
-  if (inScroll === false) {
-    inScroll = true;
-    const position = sectionEq * -100;
-    display.css({
-      transform: `translateY(${position}%)`,
-    });
-    sections.eq(sectionEq).addClass("active").siblings().removeClass("active");
-    setTimeout(() => {
-      inScroll = false;
-    }, 800);
-  }
-};
-// const viewportScroller = () => {
-//   const activeSection = sections.filter(".active");
-//   const nextSection = activeSection.next();
-//   const prevSection = activeSection.prev();
-//   return {
-//     next() {
-//       if (nextSection.length) {
-//         performTransition(nextSection.index());
-//       }
-//     },
-//     prev() {
-//       if (prevSection.length) {
-//         performTransition(prevSection.index());
-//       }
-//     },
-//   };
-// };
-// $(window).on("wheel", (e) => {
-//   const deltaY = e.originalEvent.deltaY;
-//   const scroller = viewportScroller();
-//   // let map = $('.map')
-//   // console.log(map)
-//   if (deltaY > 0) {
-//     //&& e.target != map
-//     //next
-//     scroller.next();
-//   }
-//   if (deltaY < 0) {
-//     //prev
-//     scroller.prev();
-//   }
-// });
-// $(window).on("keydown", (e) => {
-//   const tagName = e.target.tagName.toLowerCase();
-//   const scroller = viewportScroller();
-//   if (tagName != "textarea" && tagName != "input") {
-//     switch (e.key) {
-//       case 38:
-//         scroller.prev();
-//         break;
-//       case 40:
-//         scroller.next();
-//         break;
-//     }
-//   }
-// });
-
-// $("[data-scroll-to]").on('click',(e) => {
-//   e.preventDefault();
-//   const $this = $(e.currentTarget);
-//   const target = $this.attr("data-scroll-to");
-//   const reqSection = $(`[data-section-id = ${target}]`);
-
-//   performTransition(reqSection.index());
-// });
-
-// //https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
-// $(function () {
-//   $("body").swipe({
-
-//     //Generic swipe handler for all directions
-//     swipe: function (event, direction) {
-//       const scroller = viewportScroller();
-//       let scrollDirection = "";
-//       if (direction === "up") {
-//         scrollDirection = "next";
-//       }
-//       if (direction === "down") {
-//         scrollDirection = "prev";
-//       }
-//       scroller[scrollDirection]();
-//     },
-//   });
-// });
--- src/js/player.js
+++ src/js/player.js
@@ -1,95 +1,3 @@
-// let player;
-// const playerContainer = $(".player");
-// const playerStart = $(".player__start");
-// const playerSplash = $(".player__splash");
-// ////play/pause////
-
-// ////seconds to seconds:minutes////
-// const formatTime = (timeSec) => {
-//   const roundTime = Math.round(timeSec);
-
-//   const minutes = addZero(Math.floor(roundTime / 60));
-//   const seconds = addZero(roundTime - minutes * 60);
-
-//   function addZero(num) {
-//     return num < 10 ? `0${num}` : num;
-//   }
-
-//   return `${minutes} : ${seconds}`;
-// };
-// ////^seconds to seconds:minutes////
-
-// ////duration////
-// const onPlayerReady = () => {
-//   const durationSec = player.getDuration();
-
-//   $(".player__duration-estimate").text(formatTime(durationSec));
-//   if (typeof interval !== "undefined") {
-//     clearInterval(interval);
-//   }
-//   interval = setInterval(() => {
-//     const completedSec = player.getCurrentTime();
-//     const completedPercent = (completedSec / durationSec) * 100;
-//     $(".player__playback-button").css({
-//       left: `${completedPercent}%`,
-//     });
-//     $(".player__duration-completed").text(formatTime(completedSec));
-//   }, 1000);
-// };
-// ////^duration////
-
-// ////playbackButtonMove////
-// $(".player__playback").click((e) => {
-//   const bar = $(e.currentTarget);
-//   const clickedPosition = e.originalEvent.layerX;
-//   const newButtonPositionPercent = (clickedPosition / bar.width()) * 100;
-//   const newPlaybackPositionSec =
-//     (player.getDuration() / 100) * newButtonPositionPercent;
-
-//   $(".player__playback-button").css({
-//     left: `${newButtonPositionPercent}%`,
-//   });
-
-//   player.seekTo(newPlaybackPositionSec);
-// });
-// ////^playbackButtonMove////
-
-// function onYouTubeIframeAPIReady() {
-//   player = new YT.Player("yt-player", {
-//     height: "405",
-//     width: "660",
-//     videoId: "TxZwqVTaCmA",
-//     events: {
-//       onReady: onPlayerReady,
-//       // onStateChange: onPlayerStateChange
-//     },
-//     playerVars: {
-//       controls: 0,
-//       disablekb: 0,
-//       shoinfo: 0,
-//       rel: 0,
-//       autoplay: 0,
-//       modestbranding: 0,
-//     },
-//   });
-// }
-// let eventsInit = () => {
-//   $(".player__start").click((e) => {
-//     e.preventDefault();
-//     if (playerStart.hasClass("player__start--paused")) {
-//       playerStart. oveClass("player__start--paused");
-//       playerSplash. oveClass("player__splash--active");
-//       player.pauseVideo();
-//     } else {
-//       playerStart.addClass("player__start--paused");
-//       playerSplash.addClass("player__splash--active");
-//       player.playVideo();
-//     }
-//   });
-// };
-// ////^play/pause////
-
-// eventsInit();
 
 //videoplayer
 const player = $("#video-player")[0];
@@ -115,7 +23,7 @@ volumeLevel[0].value = volumeLevel[0].max;
 //уровень громкости
 
 //кнопка громкости
-const soundOf = () => {
+const soundOff = () => {
   if (player.volume === 0) {
     player.volume = soundLevel;
     volumeLevel[0].value = volumeLevel[0].max;
@@ -131,7 +39,7 @@ const soundOf = () => {
 };
 
 volumeButton.on("click", (e) => {
-  soundOf();
+  soundOff();
 
 });
 //кнопка кгросомти
